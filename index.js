7bd7c052d131dd03d794b4d57c45f93d0fb41d6a
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-01-20 06:45:17 +0530

    Finished activity with bonus

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function newImage(url, left, bottom) {
 
 function newItem(url, left, bottom) {
     let item = newImage(url, left, bottom);
+
     item.addEventListener("click", function () {
         item.remove();
         addInventory(url);
